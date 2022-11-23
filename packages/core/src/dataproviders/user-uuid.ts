3490d9278d3406047ca02883018bf7ba28908149
Klim Markelov <klim.markelov@elastic.co>
2022-11-23 12:04:50 +0100

    Delete user-uuid from data providers

--- packages/core/src/dataproviders/user-uuid.ts
+++
@@ -1,9 +0,0 @@
-import { TrackerEventProperties, TrackerEventType } from "../types";
-import { visitorId } from "../util/session";
-
-export default (
-  eventType: TrackerEventType,
-  properties: TrackerEventProperties
-) => {
-  return { ...properties, user_uuid: visitorId() };
-};
