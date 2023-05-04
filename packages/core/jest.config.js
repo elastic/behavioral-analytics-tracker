8ebe2f3e6a947a02fd41b89b855d56407fddc9d0
Yan Savitski <yan.savitski@elastic.co>
2023-05-04 11:54:06 +0200

    Update jest config

--- packages/core/jest.config.js
+++ packages/core/jest.config.js
@@ -1,7 +1,21 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
+export default {
+  testEnvironment: "jsdom",
   testTimeout: 70000,
+  preset: "ts-jest/presets/default-esm", // or other ESM presets
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
   resetMocks: true,
   setupFilesAfterEnv: ["<rootDir>/test/support.ts"],
+  transform: {
+    // '^.+\\.[tj]sx?$' to process js/ts with `ts-jest`
+    // '^.+\\.m?[tj]sx?$' to process js/ts/mjs/mts with `ts-jest`
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
+  },
 };
+
